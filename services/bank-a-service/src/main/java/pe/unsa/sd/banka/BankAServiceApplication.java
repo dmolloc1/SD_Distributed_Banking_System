38a12edb60fc7bf71d8bc1c3c9595d9040441e01
DeniseHuacani <dhuacanij@unsa.edu.pe>
2026-07-05 21:31:52 -0500

    feat(bank-a): implement AccountOperationService and integration support

--- services/bank-a-service/src/main/java/pe/unsa/sd/banka/BankAServiceApplication.java
+++ services/bank-a-service/src/main/java/pe/unsa/sd/banka/BankAServiceApplication.java
@@ -3,7 +3,7 @@ package pe.unsa.sd.banka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"pe.unsa.sd.banka", "pe.unsa.sd.persistence"})
 public class BankAServiceApplication {
 
     public static void main(String[] args) {
